6623940454299de32cb63d4fe6c3d87d2a29e772
CF20852 <CF20852@users.noreply.github.com>
2024-10-15 11:30:38 -0700

    Update LilyGo_SIM7000_GNSS_Tracker.ino
    Rewrote code to more reliably recover from a lost connection or failure to connect on the first attempt.

--- LilyGo_SIM7000_GNSS_Tracker.ino
+++ LilyGo_SIM7000_GNSS_Tracker.ino
@@ -3,16 +3,17 @@ This is a program to demonstrate the use of SIM7000 MQTT commands to send
 data to Adafruit IO.  You can set up a free Adafruit IO account to test it.
 
 The SIM7000 MQTT commands are documented in the following SIMCom application note:
-https://simcom.ee/documents/SIM7000x/SIM7000%20Series_MQTT_Application%20Note_V1.00.pdf
+https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/blob/098ee1f9f1405eb8e8e250e660815a50cbd76a51/docs/SIM7000/SIM7000%20Series_MQTT(S)_Application%20Note_V1.02.pdf
 
 They are not documented in the SIM7000 AT command manual at:
-https://simcom.ee/documents/SIM7000x/SIM7000%20Series_AT%20Command%20Manual_V1.04.pdf
+https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/blob/098ee1f9f1405eb8e8e250e660815a50cbd76a51/docs/SIM7000/SIM7000%20Series_AT%20Command%20Manual_V1.06.pdf
 
 I strongly recommend you have a copy of the above two documents close at hand when
 reading this program.
 
 For the full set of SIM7000 documentation see:
-https://simcom.ee/documents/?dir=SIM7000x
+https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/tree/098ee1f9f1405eb8e8e250e660815a50cbd76a51/docs/SIM7000
+
 
 I used the TinyGSM library code as a starting point for this program.  But the only functions
 from that library that this program uses are the GPS-related functions.  I used a
@@ -24,8 +25,8 @@ and publish data to the broker.
 This program is currently set up to work with a Hologram SIM.  It seems to work on the
 T-Mobile and AT&T networks in the USA, or at least in Prescott and Sedona, Arizona.
 
-This version of the program is set up to report the cellular network received signal strength
-indicator (RSSI) and GPS speed on the Adafruit IO feeds.   
+This version of the program is set up to report the cellular network reference signal signal-to
+noise ratio (RSSNR) and GPS speed on the Adafruit IO feeds.   
 
 Copyright 2024 Robert F. Fleming, III
 
@@ -65,12 +66,15 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // While signed into your account, you can click on the gold circle with the
 // black key in the upper right corner of the Adafruit IO web page to get your
 // IO_USERNAME (mqttUser[]) and IO_KEY (mqtt_Pass) and fill them in below.
+// Also change the mqttFeed names if you need to.
 const char* broker = "io.adafruit.com";
 const uint16_t port = 1883;
 const char mqttUser[] = "<your Adafruit IO_USERNAME>";
 const char mqttPass[] = "<your Adafruit IO_KEY>";
-const char mqttFeed1[] = "rssi";
+const char mqttFeed1[] = "rssnr";
 const char mqttFeed2[] = "speed";
+
+bool restarting = false;
 
 // The TinyGSM library is only used for the GPS functions, which I'm too
 // lazy to try to reinvent right now.
@@ -84,9 +88,10 @@ unsigned long t_zero, t_one;
 // the first token  returned by strtok()is the command just sent.
 // In that case, we want to skip everything up to the first newline
 // and then start processing.
-char * checkResponse(uint8_t tokenNr, char * delimiters, bool skipCmd) {
-  
-  while (Serial1.available() == 0) {}     //wait for data available
+char * checkResponse(uint8_t tokenNr, char * delimiters, bool skipCmd, unsigned long waitTime) {
+  unsigned long t_0 = millis();
+
+  while ( (Serial1.available() == 0) && (millis() < t_0 + waitTime) ) {}     //wait for data available
   String response = Serial1.readString();
 
   // Serial.print("Response from Serial1 = ");
@@ -126,7 +131,7 @@ char * checkResponse(uint8_t tokenNr, char * delimiters, bool skipCmd) {
 // When the SIM7000 first comes out of reset, it spits out what may
 // be some Chinese characters before it starts responding to AT commands
 // with an "OK" response.  This function waits for the "OK".
-boolean areWeAwakeYet() {
+bool areWeAwakeYet() {
   // Send the modem "AT".  Read the string from the SIM7000.  If it contains "OK",
   // the SIM7000 is awake.
   unsigned long t_0 = millis();
@@ -156,21 +161,15 @@ boolean areWeAwakeYet() {
 // This function issues the commands necessary to first connect the SIM7000
 // to an LTE CAT-M or NB-IoT network, then issues the commands necessary
 // to connect the SIM7000 to the Adafuit IO MQTT broker.
-boolean bringMQTTOnline(void) {
+bool bringMQTTOnline(void) {
   char buf[BUFSIZE1];
   char nullDelimiter[1] = "";
   char nlDelimiter[2] = "\n";
   char result[6] = "";
 
-  //Serial1.println("AT+CFUN=1,1");
-  while (!areWeAwakeYet()) {};
-
-  delay(5000);
-
   Serial1.println("ATE1");
-  //printResponse(2000);
 
-  strcpy(result, checkResponse(1, nullDelimiter, true));
+  strcpy(result, checkResponse(1, nullDelimiter, true, 2000));
   if (strstr(result, "OK") == NULL) {
     Serial.println("SIM7000 didn't respond with \"OK\" to \"ATE1\"");
     return false;
@@ -184,7 +183,7 @@ boolean bringMQTTOnline(void) {
 
   Serial1.println("AT+SGPIO=0,4,1,1");
 
-  strcpy(result, checkResponse(1, nlDelimiter, true));
+  strcpy(result, checkResponse(1, nlDelimiter, true, 2000));
   if (strstr(result, "OK") != NULL) {Serial.println("GPS power supply enabled!"); }
 
   delay(2000);
@@ -202,8 +201,8 @@ boolean bringMQTTOnline(void) {
   Serial1.println("AT+CNMP=51");
   printResponse(1000);
 
-  // AT+CMNB=3  set preferred connection type to CAT-M and NB-IoT
-  Serial1.println("AT+CMNB=3");
+  // AT+CMNB=1  set preferred connection type to CAT-M1, which supports tower handoff
+  Serial1.println("AT+CMNB=1");
   printResponse(2000);
 
   // AT+CIPMUX=1
@@ -225,9 +224,13 @@ boolean bringMQTTOnline(void) {
   Serial1.println("AT+COPS?");
   printResponse(1000);
 
-  // AT+CGNAPN  query CAT-M or NB-IOT network after the successful registration of APN
+  // AT+CGNAPN  query CAT-M1 or NB-IoT network after the successful registration of APN
   // Basically, if we get a response like +CGNAPN: 1,"hologram" back, we're connected
   Serial1.println("AT+CGNAPN");
+  printResponse(1000);
+
+  // AT+CGNSHOR=10  set the desired GNSS accuracy to 10 meters
+  Serial1.println("AT+CGNSHOR=10");
   printResponse(1000);
 
   // AT+SMCONF="URL","io.adafruit.com"
@@ -246,14 +249,20 @@ boolean bringMQTTOnline(void) {
 
   // AT+SMCONN
   Serial1.println("AT+SMCONN");
-  printResponse(10000);
-
+  strcpy(result, checkResponse(1, nullDelimiter, true, 10000));
+  if (strstr(result, "ERROR") != NULL) {
+    Serial.println("SIM7000 didn't connect to MQTT broker :-(");
+    return false;
+  }
+  if (strstr(result, "OK") != NULL) {
+    Serial.println("SIM7000 is connected to MQTT broker!");
+  }
   return true;
 }
 
 // This function parses the SIM7000 +CPSI response to retrieve the
-// Received Signal Strength Indicator (RSSI) value
-int getRSSI(char * cpsiResp) {
+// Received Signal Strength Indicator (RSSNR) value
+int getRSSNR(char * cpsiResp) {
   char respCopy[BUFSIZE3];
   char * p_respCopy = respCopy;
   strcpy(respCopy, cpsiResp);
@@ -287,23 +296,23 @@ void updateParameter() {
     // You may want to handle this case differently
   }
 
-  // Get RSSI
+  // Get RSSNR
   Serial1.println("AT+CPSI?");
   char delimiters[2] = ",";
 
   char result[6] = "";
-  strcpy(result, checkResponse(14, delimiters, true));
-  int rssi = atoi(result);
+  strcpy(result, checkResponse(14, delimiters, true, 2000));
+  int rssnr = atoi(result);
 
   char buf1[BUFSIZE1] = "";
-  snprintf(buf1, BUFSIZE1, "RSSI = %d, lat = %2.6f, lon = %3.6f, alt = %4.1f", rssi, latitude, longitude, altitude);
+  snprintf(buf1, BUFSIZE1, "RSSNR = %d, lat = %2.6f, lon = %3.6f, alt = %4.1f", rssnr, latitude, longitude, altitude);
   Serial.println(buf1);
 
-  // Prepare MQTT publish content for RSSI
+  // Prepare MQTT publish content for RSSNR
   // Refer to the SIMCom SIM7000 MQTT appnote to see how to do this.
   // First you issue the command, then the SIM7000 prompts for the data to publish.
   char buf2[BUFSIZE2] = "";
-  int size2 = snprintf(buf2, BUFSIZE2, "%d,%2.6f,%3.6f,%4.1f", rssi, latitude, longitude, altitude);
+  int size2 = snprintf(buf2, BUFSIZE2, "%d,%2.6f,%3.6f,%4.1f", rssnr, latitude, longitude, altitude);
   strncpy(buf1, "", sizeof(buf1));
   snprintf(buf1, BUFSIZE1, "AT+SMPUB=\"cf20855/feeds/%s/csv\",\"%d\",1,1", mqttFeed1, size2);
   
@@ -341,10 +350,16 @@ void printResponse(int delay) {
   }
 }
 
-void setup() {
+void setup() {  
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
+
+  Serial.println("ESP32 is awake.");
+
   // Power cycle the SIM7000G
   // Note that there is an inverter between the ESP32 output and the SIM7000
   // PWRKEY pin.
+  Serial.println("Powering up SIM7000...");
   pinMode(PWR_PIN, OUTPUT);
   digitalWrite(PWR_PIN, HIGH);
   delay(2000);
@@ -353,9 +368,6 @@ void setup() {
   digitalWrite(PWR_PIN, HIGH);
   // According to the datasheet the SIM7000 takes 4.5 seconds to become active after powerup
   delay(5000);
-
-  Serial.begin(115200);
-  Serial1.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
 
   // Wait for the SIM7000 to wake up
   if (areWeAwakeYet()) {
@@ -375,15 +387,15 @@ void setup() {
 }
 
 void loop() {
-  // The following two "if" statements are useful for manual control
-  // of the SIM7000.
-  // if (Serial.available()) {      // If anything comes in Serial (USB),
-  //   Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-  // }
+  //The following two "if" statements are useful for manual control
+  //of the SIM7000.
+  if (Serial.available()) {      // If anything comes in Serial (USB),
+    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
+  }
 
-  // if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-  //   Serial.write(Serial1.read());   // read it and send it out Serial (USB)
-  // }
+  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
+    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
+  }
 
   // Check the connection status every 13 seconds
   if (millis() > t_zero + 13000) {
@@ -392,7 +404,7 @@ void loop() {
     char delimiters[2] = ":";
 
     char result[6] = "";
-    strcpy(result, checkResponse(2, delimiters, true));
+    strcpy(result, checkResponse(2, delimiters, true, 2000));
 
     // Serial.print("MQTT state response token = ");
     // Serial.println(result);
